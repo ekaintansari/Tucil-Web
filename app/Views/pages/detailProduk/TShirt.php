65fccb81594c2a061757cab6660bdf76e45ead70
eka <ekaintan225@gmail.com>
2020-11-19 22:26:29 +0700

    Tucil

---
+++ app/Views/pages/detailProduk/TShirt.php
@@ -0,0 +1,9 @@
+<?= $this->extend('layout/app'); ?>
+<?= $this->section('content'); ?>
+<div class="col">
+    <h3>TShirt</h3>
+</div>
+<p>Tshirt adalah sebuah kaos yang dijual pada toko kami dengan bahan yang nyaman bagi pembelinya.
+
+</p>
+<?= $this->endSection(); ?>
